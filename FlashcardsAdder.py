d17d91226c3074b834cb51a323dcf8d7f1a91113
rr <rr@pop-os.localdomain>
2020-11-08 22:38:09 +0100

    Reworked FlashcardsAdder file

--- FlashcardsAdder.py
+++ FlashcardsAdder.py
@@ -6,12 +6,11 @@ from tkinter import ttk
 
 # class FlashcardsMenuFunctions(Sql_db):
 class FlashcardsMenuFunctions():
-    def __init__(self):
-        self.db_connector = Sql_db()
+    db_connector = Sql_db()
 
+    def __init__(self):
+        # self.db_connector = Sql_db()
         self.db_connector.connect_db()
-        # self.connect_db()
-        # self.create_db_if_not_exists()
         self.db_connector.create_db_if_not_exists()
 
     def add_to_db(self):
@@ -19,88 +18,95 @@ class FlashcardsMenuFunctions():
         _meaning = self.meaning_entry.get()
         _note = self.note_entry.get()
         print(_word, _meaning, _note)
-        # self.insert_to_db(_word, _meaning, _note)
         self.db_connector.insert_to_db(_word, _meaning, _note)
 
     def pass_to_remove(self):
         item_to_delete = self.tree.item(self.tree.focus())
-        # print(item_to_delete)
+
         _word = item_to_delete["values"][0]
 
         _meaning = item_to_delete["values"][1]
 
         _note = item_to_delete["values"][2]
 
-        # self.remove_from_db( _word, _meaning, _note)
         self.db_connector.remove_from_db( _word, _meaning, _note)
-        # self.myresult = self.mydb.commit()
+
         self.scrollbar.destroy()
         self.tree.destroy()
         self.create_treeview()
 
          
     def send_edited_to_db(self, word_before, meaning_before, note_before):
-        print(self.window.winfo_children())
+        print(self.window.app.winfo_children())
         _edited_word = self.word_entry.get()
         _edited_meaning = self.meaning_entry.get()
         _edited_note = self.note_entry.get()
 
-        # self.edit_db(word_before, meaning_before, note_before, _edited_word, _edited_meaning, _edited_note)
         self.db_connector.edit_db(word_before, meaning_before, note_before, _edited_word, _edited_meaning, _edited_note)
 
     def load_flashcards(self):
         db_records = self.db_connector.get_from_db("*","flashcards_examples")
         return db_records
 
+    def clear_entries(self):
+        self.word_entry.delete(0, 'end')
+        self.meaning_entry.delete(0, 'end')
+        self.note_entry.delete(0, 'end')
+
+    def add_word_button_function(self):
+        self.add_to_db()
+        self.clear_entries()
+        self.changes_applied_visible()
+
+    def edit_word_button_function(self, edited_word, edited_meaning, edited_note):
+        self.send_edited_to_db(edited_word, edited_meaning, edited_note)
+        self.clear_entries()
+        self.changes_applied_visible()
+
 #################################################################################################################################
 
 class FlashcardsMenu(FlashcardsMenuFunctions):
-    def __init__(self, window, menu, clear_window):
+    tree_buttons = False
+    edit_var = False
+
+    def __init__(self, window, menu_layout):
         super().__init__()
-        self.passed_clear_window_function = clear_window
-        self.menu = menu
+        self.menu_layout = menu_layout
         self.window = window
-
-        # self.mycursor = self.mydb.cursor()
-        self.tree_buttons = False
-        self.edit_var = False
-        
-
+  
 
     def goto_main(self):
-        self.menu.render_buttons()
-        self.menu.grid_buttons()
-        self.menu.create_label()
+        self.menu_layout.render_buttons()
+        self.menu_layout.grid_buttons()
+        self.menu_layout.create_label()
 
 
     def create_buttons(self):
         self.back = tkinter.Button(text="Go back", command =lambda: self.goto_main())
-        self.add_word = tkinter.Button(text="Add word", command = lambda: [self.add_to_db(), self.clear_entries(), self.changes_applied_visible()])
+        self.add_word = tkinter.Button(text="Add word", command = lambda: self.add_word_button_function())
         self.change_info = tkinter.Label(text="Waiting for changes")
-        #width=width//3
 
 
     def changes_applied_visible(self):
         self.change_info.configure(text="Changes applied")
 
     def create_labels(self):
-        self.word = tkinter.Label(self.window, text="Word")
-        self.meaning = tkinter.Label(self.window, text="Meaning")
-        self.note = tkinter.Label(self.window, text="Note")
+        self.word_label = tkinter.Label(self.window.app, text="Word")
+        self.meaning_label = tkinter.Label(self.window.app, text="Meaning")
+        self.note_label = tkinter.Label(self.window.app, text="Note")
 
 
     def place_entries(self):
-        self.word_entry = tkinter.Entry(self.window, font=('Arial 24'))
-        self.meaning_entry = tkinter.Entry(self.window, font=('Arial 24'))
-        self.note_entry = tkinter.Entry(self.window, font=('Arial 24'))
-        print(self.window)
+        self.word_entry = tkinter.Entry(self.window.app, font=('Arial 24'))
+        self.meaning_entry = tkinter.Entry(self.window.app, font=('Arial 24'))
+        self.note_entry = tkinter.Entry(self.window.app, font=('Arial 24'))
 
     def pack_layout(self):
-        self.word.pack(fill='both', expand=True)
+        self.word_label.pack(fill='both', expand=True)
         self.word_entry.pack(fill='both', expand=True)
-        self.meaning.pack(fill='both', expand=True)
+        self.meaning_label.pack(fill='both', expand=True)
         self.meaning_entry.pack(fill='both', expand=True)
-        self.note.pack(fill='both', expand=True)
+        self.note_label.pack(fill='both', expand=True)
         self.note_entry.pack(fill='both', expand=True)
 
         self.back.pack(fill='both', expand=True, side='left')
@@ -108,15 +114,10 @@ class FlashcardsMenu(FlashcardsMenuFunctions):
         self.add_word.pack(fill='both', expand=True, side='left')
         
 
-    def clear_entries(self):
-        self.word_entry.delete(0, 'end')
-        self.meaning_entry.delete(0, 'end')
-        self.note_entry.delete(0, 'end')
-
 
     def goto_flashcards_adder(self):
         self.edit_var = False
-        self.passed_clear_window_function()
+        self.window.clear_window()
         self.create_labels()
         self.place_entries()
         self.create_buttons()
@@ -124,9 +125,11 @@ class FlashcardsMenu(FlashcardsMenuFunctions):
 
 
     def create_treeview_buttons(self):
-        window_width = self.window.winfo_width()
-        button_frame = tkinter.Frame(self.window, width = int(window_width*0.25))
-        self.add = tkinter.Button(button_frame, text="Add", command=lambda: [self.goto_flashcards_adder()])
+        window_width = self.window.app.winfo_width()
+
+        button_frame = tkinter.Frame(self.window.app)
+
+        self.add = tkinter.Button(button_frame, text="Add", command=lambda: self.goto_flashcards_adder())
         self.add.pack(fill="both", expand=True)
 
         self.edit = tkinter.Button(button_frame, text="Edit", command=lambda: self.edit_record())
@@ -142,14 +145,13 @@ class FlashcardsMenu(FlashcardsMenuFunctions):
 
 
     def treeview(self):
-        self.treeframe = tkinter.Frame(self.window)
+        self.treeframe = tkinter.Frame(self.window.app)
         self.create_treeview()
         self.create_treeview_buttons()
         self.treeframe.pack(side="left", fill="both", expand=True)
 
     def create_treeview(self):
         self.tree = ttk.Treeview(self.treeframe)
-
 
         self.scrollbar = tkinter.Scrollbar(self.treeframe, orient="vertical", command=self.tree.yview)
         self.tree.configure(yscrollcommand=self.scrollbar.set)
@@ -165,10 +167,7 @@ class FlashcardsMenu(FlashcardsMenuFunctions):
         self.tree.heading("three", text="Note",anchor=tkinter.W)
 
 
-        # self.myresult = self.get_from_db("*","flashcards_examples")
-        
 
-        # for value, x in enumerate(self.myresult):
         for value, x in enumerate(self.load_flashcards()):
             self.tree.insert(parent="", index=value, values=(f"{x[1]}",f"{x[2]}",f"{x[3]}"))
         self.tree.pack(fill="both", expand=True, side="left")
@@ -176,11 +175,14 @@ class FlashcardsMenu(FlashcardsMenuFunctions):
 
     def edit_record(self):
         item_to_edit = self.tree.item(self.tree.focus())
-        self.passed_clear_window_function()
+
+        self.window.clear_window()
         self.goto_flashcards_adder()
 
         self.add_word.destroy()
-        self.edit_word = tkinter.Button(text="Edit", command=lambda: [self.send_edited_to_db(item_to_edit['values'][0], item_to_edit['values'][1], item_to_edit['values'][2]), self.clear_entries(), self.changes_applied_visible()])
+        # self.edit_word = tkinter.Button(text="Edit", command=lambda: [self.send_edited_to_db(item_to_edit['values'][0], item_to_edit['values'][1], item_to_edit['values'][2]), self.clear_entries(), self.changes_applied_visible()])
+        self.edit_word = tkinter.Button(text="Edit", command=lambda: self.edit_word_button_function(item_to_edit['values'][0], item_to_edit['values'][1], item_to_edit['values'][2]))
+
         self.edit_word.pack(fill='both', expand=True, side='right')
 
 
@@ -192,10 +194,5 @@ class FlashcardsMenu(FlashcardsMenuFunctions):
 
         self.note_entry.insert(0, f"{item_to_edit['values'][2]}")
 
-
-
-
-    def refresh(self):
-        self.clear_entries()
 
 
